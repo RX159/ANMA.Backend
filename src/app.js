47326226d5831daddc2265c1c92fcef2d72b6e9d
Ricardo Reyes <alcala_ricardo@hotmail.com>
2020-03-06 22:06:50 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
 const request = require('request')
+const express = require('express')
+const router = require('./routes')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json()) // parsea a json
+app.use(router)
+
+
+app.listen(port, function() {
+  console.log('Server up and running on port ' + port)
+})
+
 
 var markers = new Array
 //GoogleAPIKEY = 'AIzaSyAqfVfnIq48fSqyiSuNUbc2txi0a7IVbzY'
